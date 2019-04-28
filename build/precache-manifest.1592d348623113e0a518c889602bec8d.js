d1795f39fb916b88379d8188a8ca2422b1714be5
mtgoodrich <development@matthewgoodrich.com>
2019-04-27 20:53:04 -0700

    Use build folder for site

---
+++ build/precache-manifest.1592d348623113e0a518c889602bec8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ececbf779ec4cb2988",
+    "url": "/cat-egory/static/css/main.79b3f5e2.chunk.css"
+  },
+  {
+    "revision": "13ececbf779ec4cb2988",
+    "url": "/cat-egory/static/js/main.6932ff18.chunk.js"
+  },
+  {
+    "revision": "378f06187431084d05a2",
+    "url": "/cat-egory/static/js/runtime~main.5e3d575d.js"
+  },
+  {
+    "revision": "fd320b869fdfb23d0956",
+    "url": "/cat-egory/static/js/2.b4d097f5.chunk.js"
+  },
+  {
+    "revision": "ee426896ca9efd9d3bab72aca83c6fa0",
+    "url": "/cat-egory/index.html"
+  }
+];
